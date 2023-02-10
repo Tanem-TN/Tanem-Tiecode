932592c45a5df128c237a048ce2159746153bbca
Tanem-TN <1439027486@qq.com>
2023-02-11 03:35:13 +0800

    更新api

--- app/src/main/java/com/example/tiecodeluntan/MainActivity.java
+++ app/src/main/java/com/example/tiecodeluntan/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tiecodeluntan;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.example.tiecodeluntan.Pager.PagerAdapter;
 
 import cn.zhxu.data.Mapper;
+import cn.zhxu.okhttps.HTTP;
 import cn.zhxu.okhttps.HttpCall;
 import cn.zhxu.okhttps.HttpResult;
 import cn.zhxu.okhttps.OkHttps;
@@ -25,11 +27,15 @@ public class MainActivity extends AppCompatActivity {
         ViewPager2 vPager = findViewById(R.id.Pager1);
         RecyclerView.Adapter PagerAdapter = new PagerAdapter(this);
         vPager.setAdapter(PagerAdapter);
-        HttpCall httpCall = OkHttps.async("/users/1")
+        HTTP htpp = HTTP.builder().build();
+        htpp.async("http://101.33.227.148:80/api")
+                .bodyType("application/json; charset=utf-8")
+                .addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78")
                 .setOnResponse((HttpResult res) -> {
                     // 得到目标数据
                     HttpResult.Body boy = res.getBody();
+                    Log.i("htpps",boy.toString());
                 })
                 .get();
-    }
+  }
 }
